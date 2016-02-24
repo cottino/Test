341276a132ba2eb162fa94e5acebfc10caa090ca
ChrisOttino <ottinochris@gmail.com>
2016-02-23 17:01:52 -0700

    commiting from test branch

--- Test/src/TestMain.java
+++ Test/src/TestMain.java
@@ -2,5 +2,6 @@ import java.util.*;
 public class TestMain {
 	public static void main(String[] args){
 		System.out.println("Hello World");
+		System.out.println("merging main from chrisTestBranch to make sure it is as easy as it looks");
 	}
 }
