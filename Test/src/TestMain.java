9cb9b610fe20b6373cb7f5686ccefe0544858999
ChrisOttino <ottinochris@gmail.com>
2016-02-23 17:05:30 -0700

    trying to merge the bastard

--- Test/src/TestMain.java
+++ Test/src/TestMain.java
@@ -3,5 +3,6 @@ public class TestMain {
 	public static void main(String[] args){
 		System.out.println("Hello World");
 		System.out.println("merging main from chrisTestBranch to make sure it is as easy as it looks");
+		System.out.println("common now");
 	}
 }
